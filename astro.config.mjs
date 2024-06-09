dec09e8d6ce73a8345110169c30496dabd39542f
Samuel1401s <gomez.velasquez.samuel@gmail.com>
2024-06-09 12:03:01 -0500

    de los ultimos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Samuel1401s.github.io',
+  base: 'Project',
+});
